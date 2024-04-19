a3cc11e90e6445524c18e2adb254892e92f4cd64
AbduHany <abdu.hany@gmail.com>
2024-04-19 16:12:41 +0200

    Modify version installed: 1-install_a_package.pp

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
 # This manifest installs flask from pip3.
 package { 'Flaskinstaller':
-  name     => 'Flask',
-  provider => 'pip3'
+  ensure   => '2.1.0',
+  provider => 'pip3',
+  name     => 'flask',
 }
