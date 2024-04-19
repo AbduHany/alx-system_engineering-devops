bd19f61e4f9e7013335a457c940f8b3e19808a95
AbduHany <abdu.hany@gmail.com>
2024-04-19 15:52:34 +0200

    Fix syntax error 0-create_a_file.pp

--- 0x0A-configuration_management/0-create_a_file.pp
+++ 0x0A-configuration_management/0-create_a_file.pp
@@ -1,8 +1,7 @@
-file { 'school_file'
-  path => '/tmp/school'
-  ensure => file
-  mode => '0744'
-  owner => 'www-data'
-  group => 'www-data'
-  content => 'I love Puppet'
+file { 'createschool':
+  path => '/tmp/school',
+  mode => '0744',
+  owner => 'www-data',
+  group => 'www-data',
+  content => 'I love Puppet',
 }
