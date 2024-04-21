1ab6c228c55907f67520e3ae7a31d6bc7d7084d9
AbduHany <abdu.hany@gmail.com>
2024-04-21 13:44:18 +0200

    Modify 100-puppet_ssh_config.pp

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,7 +1,8 @@
 # This puppet script make changes to the local
 # ssh client configuration file.
-file { 'ssh_config':
-  path => '/etc/ssh/ssh_config',
-  ensure => 'file',
-  content => 'Host *\n\tIdentityFile ~/.ssh/school\n\tBatchMode yes',
+file { "ssh_config":
+  path => "/etc/ssh/ssh_config",
+  content => "Host *
+  IdentityFile ~/.ssh/school
+  BatchMode yes",
 }
