60e8f5cb8094328f2bdec1bc963686755567b6c1
AbduHany <abdu.hany@gmail.com>
2024-04-30 02:32:01 +0300

    Modify 2-puppet_custom_http_response_header.pp

--- 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
+++ 0x0F-load_balancer/2-puppet_custom_http_response_header.pp
@@ -1,21 +1,10 @@
 # This manifest configures brand new Ubuntu machine to install
 # NGINX and make it return a X-Served-By response header with
 # the value of the server's hostname.
-exec { 'update_apt':
-  command  => 'apt-get update',
-  provider => shell,
-}
-package { 'nginx_installer':
-  name     => 'nginx',
-  provider => 'apt',
-}
-exec { 'addheader':
-  command => "sed -i \
-'s#root /var/www/html;#root /var/www/html;\n\n\tadd_header X-Served-By $HOSTNAME;\n#' \
-/etc/nginx/sites-available/default",
-  provider => shell,
-}
-exec { 'restart_nginx':
-  command  => 'service nginx restart',
+exec { 'setup':
+  command  => 'sudo apt update;
+  sudo apt-get install -y nginx;
+  sudo sed -i "/root /var/www/html;/a add_header X-Served-By $HOSTNAME;\n" /etc/nginx/sites-available/default;
+  sudo service nginx restart',
   provider => shell,
 }
