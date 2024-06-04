ae3d65f549c813e8a41d0df8613d60072f21c518
AbduHany <abdu.hany@gmail.com>
2024-06-04 13:54:07 +0300

    Modify 1-top_ten.py: use requests

--- 0x16-api_advanced/1-top_ten.py
+++ 0x16-api_advanced/1-top_ten.py
@@ -1,9 +1,7 @@
 #!/usr/bin/python3
 """This moduled defines top_ten function.
 """
-import urllib.request
-import urllib.parse
-import json
+import requests
 
 
 def top_ten(subreddit):
@@ -11,14 +9,14 @@ def top_ten(subreddit):
     first 10 hot posts listed for a given subreddit.
     """
     url = 'https://www.reddit.com/r/{}/hot.json?count=10'.format(subreddit)
-    req = urllib.request.Request(url)
-    req.redirect = False
-    try:
-        respone = urllib.request.urlopen(req)
-    except Exception:
-        print(None)
-    obj = json.loads((respone.read().decode('utf-8')))
-    for idx in range(10):
-        if (idx >= len(obj['data']['children'])):
-            break
-        print(obj['data']['children'][idx]['data']['title'])
+    headers = {"User-Agent": "Mozilla/5.0"}
+    res = requests.get(url, headers=headers, allow_redirects=False)
+
+    if (res.status_code != 200):
+        print('None')
+    else:
+        obj = res.json()
+        for idx in range(10):
+            if (idx >= len(obj['data']['children'])):
+                break
+            print(obj['data']['children'][idx]['data']['title'])
