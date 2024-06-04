10c39ca8fcb465ef81bf1f1d1302cacdaf5477e0
AbduHany <abdu.hany@gmail.com>
2024-06-04 13:58:22 +0300

    Modify 0-subs.py: add User-Agent: app/1.0

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -10,7 +10,7 @@ def number_of_subscribers(subreddit):
     for a given subreddit.
     """
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
-    headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {"User-Agent": "app/1.0"}
     res = requests.get(url, headers=headers, allow_redirects=False)
     if (res.status_code != 200):
         return (0)
