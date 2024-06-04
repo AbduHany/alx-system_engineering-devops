43da1428cebb55b033f9580bc8b1d49759ae3c47
AbduHany <abdu.hany@gmail.com>
2024-06-04 13:44:16 +0300

    Modify 0-subs.py

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -12,8 +12,7 @@ def number_of_subscribers(subreddit):
     url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
     headers = {"User-Agent": "Mozilla/5.0"}
     res = requests.get(url, headers=headers, allow_redirects=False)
-    if (res.status_code == 302):
+    if (res.status_code != 200):
         return (0)
-    elif (res.status_code == 200):
-        obj = res.json()
-        return (obj['data']['subscribers'])
+    obj = res.json()
+    return (obj['data']['subscribers'])
