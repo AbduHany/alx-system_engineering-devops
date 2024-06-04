b719b41c3876edd63ec38b7930bac70b976bf229
AbduHany <abdu.hany@gmail.com>
2024-06-04 10:47:25 +0300

    Add 0-subs.py

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""This moduled defines function returns the number
+of subscribers for a given subreddit
+"""
+import urllib.request
+import urllib.parse
+import json
+import sys
+
+
+def number_of_subscribers(subreddit):
+    """ This function returns the number of subscribers
+    for a given subreddit.
+    """
+    url = 'https://www.reddit.com/r/{}/about.json'.format(subreddit)
+    req = urllib.request.Request(url)
+    try:
+        respone = urllib.request.urlopen(req)
+    except Exception:
+        return (0)
+    obj = json.loads((respone.read().decode('utf-8')))
+    subs = obj['data']['subscribers']
+    return(subs)
