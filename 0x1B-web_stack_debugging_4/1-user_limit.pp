735a9fcd419f23e468d30ebb86c056c7d839f38a
AbduHany <abdu.hany@gmail.com>
2024-06-13 23:08:20 +0300

    Testing

--- 0x1B-web_stack_debugging_4/1-user_limit.pp
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -2,6 +2,6 @@
 file { 'change_holberton_limits':
   ensure  => file,
   path    => '/etc/security/limits.conf',
-  content => 'holberton hard nofile 15
-holberton soft nofile 15',
+  content => 'holberton hard nofile 5
+holberton soft nofile 4',
 }
